63fa996f481dd6270efd591f6c9a556417d169ec
Shibili Aman TK <dlshacker159@gmail.com>
2025-11-21 20:43:59 +0530

    2

---
+++ public/script.js
@@ -0,0 +1,15 @@
+function generateqr() {
+    const url = document.getElementId('urlInput').value;
+    const qrresult = document.getElementById('qr-result');
+    const qrImage = document.getElementById('qrImage');
+    const dl = document.getElementById('downloadLink');
+
+    if (!url) {
+        alert("Thandhayillayima kanikkaruth");
+        return;
+    }
+    const apiUrl = `/api/index?url=${encodeURIComponent(url)}`;
+    qrImage.src = apiUrl;
+    dl.href = apiUrl;
+    qrresult.classList.remove('hidden');
+}
